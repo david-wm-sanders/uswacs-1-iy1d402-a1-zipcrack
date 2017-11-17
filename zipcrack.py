ff79b8d5fde068ffd52a3ad3c026865a77d667c1
David Sanders <david.wm.sanders@gmail.com>
2017-11-17 14:09:38 +0000

    Functionalisation

--- zipcrack.py
+++ zipcrack.py
@@ -4,24 +4,26 @@ from pathlib import Path
 
 wrd_fp = Path(__file__).parent / Path("dictionary.txt")
 zip_fp = Path(__file__).parent / Path("locked.zip")
-file_n = "picture.jpg"
 
-pws = []
-with wrd_fp.open("rb") as f:
-    for pw in f.read().split(b"\n"):
-        pws.append(pw)
+def read_word_list(wrd_fp):
+    pws = []
+    with wrd_fp.open("rb") as f:
+        for pw in f.read().split(b"\n"):
+            pws.append(pw)
+    return pws
 
-print("Wordlist:", pws)
-
-with zipfile.ZipFile(zip_fp) as z:
-    for pw in pws:
-        try:
-            with z.open(file_n, pwd=pw) as zc:
-                of = Path(__file__).parent / Path(f"{file_n}")
-                with of.open("wb") as f:
-                    print(f"The password is '{pw}' - writing file to disk...")
-                    f.write(zc.read())
+def crack_zip(zip_fp, word_list):
+    with zipfile.ZipFile(zip_fp) as z:
+        for pw in pws:
+            try:
+                z.extractall(path="extracted", pwd=pw)
+                print(f"The password is '{pw}' - writing file to disk...")
                 break
-        except RuntimeError:
-            print(f"The password is not '{pw}'")
-            continue
+            except RuntimeError:
+                print(f"The password is not '{pw}'")
+                continue
+
+if __name__ == '__main__':
+    pws = read_word_list(wrd_fp)
+    print("Wordlist:", pws)
+    crack_zip(zip_fp, pws)
